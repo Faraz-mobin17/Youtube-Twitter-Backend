2b9b83d8c9e4c980cca78000b39f043eb162f49c
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-03-21 16:39:50 +0530

    ✅ added userExists in registerUser fn, addd loginUser fn, and route, added unauth and conflict value in constant file

--- routes/users.route.js
+++ routes/users.route.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const {
   getAllUsers,
   getUser,
-  createUser,
+  registerUser,
+  loginUser,
   updateUser,
   deleteUser,
 } = require("../controllers/users.controller.js");
 /* GET users listing. */
 router.get("/", getAllUsers);
-router.post("/register", createUser);
+router.post("/register", registerUser);
+router.post("/login", loginUser);
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 module.exports = router;
