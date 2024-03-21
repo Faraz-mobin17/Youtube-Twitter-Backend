290b303e77892d3fda9cc0e3a9b0dee3f8b658d2
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-03-22 04:01:17 +0530

    ✅ user schema added and add more routes

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const router = express.Router();
-
+const verifyJWT = require("../middlewares/auth.middleware.js");
 const {
   getAllUsers,
   getUser,
   registerUser,
   loginUser,
+  logoutUser,
   updateUser,
   deleteUser,
 } = require("../controllers/users.controller.js");
 /* GET users listing. */
-router.get("/", getAllUsers);
+router.get("/", verifyJWT, getAllUsers);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+router.post("/logout", verifyJWT, logoutUser);
+router
+  .route("/:id")
+  .get(verifyJWT, getUser)
+  .patch(verifyJWT, updateUser)
+  .delete(verifyJWT, deleteUser);
 module.exports = router;
