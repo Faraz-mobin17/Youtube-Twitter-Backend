af5aa82a87bb8219ab36dbb0a6ad6698b26f7198
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-06-25 18:51:55 +0530

    ✅ added return statement and fixed imports

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,9 +1,9 @@
-import { ApiResponse, ApiError } from "../utils/ApiHandler.utils.js";
+import { ApiResponse } from "../utils/ApiHandler.utils.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const healthcheck = asyncHandler(async (req, res) => {
   //TODO: build a healthcheck response that simply returns the OK status as json with a message
-  res.status(200).json(new ApiResponse(200, "OK"));
+  return res.status(200).json(new ApiResponse(200, "OK"));
 });
 
 export { healthcheck };
